6b9aecc75dc1ad8bd51edf9ad5f4e4b1a7c7f146
Ali Khan <88495045+alik95997@users.noreply.github.com>
2025-10-20 16:14:57 +0500

    Fix BrowserRouter nesting in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
+    <BrowserRouter> 
     <App />
-    <BrowserRouter>  </BrowserRouter>
+    
+    </BrowserRouter>
   </React.StrictMode>
 );
